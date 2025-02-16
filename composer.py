2d5ba54bc40e05b64c72ee1a37380252c8a98246
Benny-JH-L <bennyliang24@gmail.com>
2025-02-15 17:06:30 -0700

    composer

--- composer.py
+++ composer.py
@@ -25,7 +25,6 @@ class Composer:
             COMPOSER_KEY_BINDS[5]: 10,  # Magnet
             COMPOSER_KEY_BINDS[6]: 10,  # Speeds up tempo
         }
-        print(self.ability_costs)
 
     def use_energy(self, amount):
         if self.sound_energy >= amount:
@@ -39,42 +38,45 @@ class Composer:
     def update(self, keys):
         self.handle_input(keys)
 
-    def handle_input(self, keys, dancer):
+    def handle_input(self, keys):
         # Super Jump - 1
         if keys[pygame.K_1]:
             if self.use_energy(self.ability_costs[1]):
-                dancer.super_jump = True
+                self.dancer.super_jump = True
         # Dash - 2
         if keys[pygame.K_s]:
             if self.use_energy(self.ability_costs[2]):
-                dancer.can_dash = True
+                self.dancer.can_dash = True
         # Blink - 3
         if keys[pygame.K_3]:
             if self.use_energy(self.ability_costs[3]):
-                dancer.blink = True
+                self.dancer.can_blink = True
         # Shield - 4
         if keys[pygame.K_4]:
             if self.use_energy(self.ability_costs[4]):
-                dancer.activate_shield = True
-        # Time Slow - 5
-        if keys[pygame.K_5]:
-            if self.use_energy(self.ability_costs[5]):
-                dancer.slow_time = True
+                self.dancer.shielded = True
         # Magnet - 6
         if keys[pygame.K_6]:
             if self.use_energy(self.ability_costs[6]):
-                dancer.activate_magnet = True
-        # Speeds up temp - 7
-        if keys[pygame.K_7]:
-            if self.use_energy(self.ability_costs[7]):
-                dancer.speed_up_tempo = True
-
-    def collect_notes(self, note_shards):
-        collected = pygame.sprite.spritecollide(self.dancer, note_shards, True)
-        if collected:
-            for shard in collected:
-                self.recharge(20)
-                # Optionally, you might want to indicate the recharge visually
+                self.dancer.activate_magnet = True
+        
+        # LEVEL WIDE AFFECTS
+        # # Speeds up temp - 7
+        # if keys[pygame.K_7]:
+        #     if self.use_energy(self.ability_costs[7]):
+        #         self.dancer.speed_up_tempo = True
+        # # Time Slow - 5
+        # if keys[pygame.K_5]:
+        #     if self.use_energy(self.ability_costs[5]):
+        #         self.dancer.slow_time = True
+        
+    # DONE IN `main.py`
+    # def collect_notes(self, note_shards):
+    #     collected = pygame.sprite.spritecollide(self.dancer, note_shards, True)
+    #     if collected:
+    #         for shard in collected:
+    #             self.recharge(20)
+    #             # Optionally, you might want to indicate the recharge visually
 
     def draw_ui(self, screen):
         # Draw sound energy meter
